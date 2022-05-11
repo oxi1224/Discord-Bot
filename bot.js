968c954d5cb14365b66adb19bf4fbf79d59c1724
ox-i <oxi1224@o2.pl>
2022-05-11 14:41:31 +0200

    move file

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-import { TOKEN } from './auth.js';
+import { TOKEN } from './src/auth.js';
 import { Client } from 'discord.js';
-import { startCommands } from './startCommands.js';
+import { startCommands } from './src/startCommands.js';
 
 export const client = new Client({ intents: 32767 });
 
