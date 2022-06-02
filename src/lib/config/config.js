9f8f787a24bb0d98178172e788b9305ae4fb47fe
ox-i <oxi1224@o2.pl>
2022-06-02 18:10:47 +0200

    did a stupid

---
+++ src/lib/config/config.js
@@ -0,0 +1,21 @@
+const config = {
+  'guild': '508779434929815554',
+  'loggingChannel': '510442164400947218',
+  'errorsChannel': '981918816739131443',
+  'mutedRole': '510866226130714624',
+  'prefix': '*',
+  'emotes': {
+    'error': '<:error:980866363461599292>',
+    'success': '<:success:980866382323396723>',
+    'info':  '<:info:980866381283201025>'
+  }
+};
+
+export const guildId = config.guild;
+export const loggingChannel = config.loggingChannel;
+export const errorsChannel = config.errorsChannel;
+export const mutedRole = config.mutedRole;
+export const errorEmote = config.emotes.error;
+export const successEmote = config.emotes.success;
+export const infoEmote = config.emotes.info;
+export const prefix = config.prefix;
