d2190f5bf1f4b31a6e5210e6c481a91199de0095
ox-i <oxi1224@o2.pl>
2022-06-02 15:45:03 +0200

    removed useless auth value

--- src/lib/common/auth.js
+++ src/lib/common/auth.js
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 export const CLIENT_ID = process.env.CLIENT_ID;
-export const GUILD_ID = process.env.GUILD_ID;
 export const TOKEN = process.env.TOKEN;
 export const CONNECTION_STRING = process.env.DATABASE_URL;
