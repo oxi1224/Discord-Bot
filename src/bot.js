15406b59eb6662121ff1a267034d271910390d9f
ox-i <oxi1224@o2.pl>
2022-06-02 15:43:11 +0200

    bot.js changes

--- src/bot.js
+++ src/bot.js
@@ -10,7 +10,7 @@ client.on('ready', async () => {
 });
 
 (async function start() {
-  await client.login(TOKEN)
+  client.isReady ? await client.login(TOKEN)
     .then(await createLogsTable())
-    .then(startCommands());
+    .then(startCommands()) : null;
 })();
