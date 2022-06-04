221737361b71eaa26761a664f75eb693883f73ad
ox-i <oxi1224@o2.pl>
2022-06-04 13:59:35 +0200

    bug testing

--- src/lib/util/util.js
+++ src/lib/util/util.js
@@ -37,7 +37,10 @@ export function getExpirationDate(duration, startTime) {
 export async function logPunishment(userId, reason, moderator, column, duration) {
   if (!(await db.existsRow(userId))) await db.createUserRow(userId);
   // get the previous punishments
-  const userPunishmentsList = await db.readFromDb(userId, 'PunishmentLogs') == undefined ? [] : (await db.readFromDb(userId, 'PunishmentLogs'))[0][column];
+  console.log(column);
+  console.log(await db.readFromDb(userId));
+  const userPunishmentsList = await db.readFromDb(userId) == undefined ? [] : (await db.readFromDb(userId))[0][column];
+  console.log(userPunishmentsList);
   const punishmentType = column.split('').slice(0, -1).join('');
   // update the punishment list
   userPunishmentsList.push({
