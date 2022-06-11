57db9c83faefdc2fc4a471a6b098192b994ac68a
ox-i <oxi1224@o2.pl>
2022-06-11 13:24:55 +0200

    Cleaned up leftover command handler code

--- src/lib/commandHandler.js
+++ src/lib/commandHandler.js
@@ -22,53 +22,6 @@ export async function appendToCommandArray({
     slash === true ? await updateSlashCommands(slashData, aliases[0]) : null;
   }
   return commands;
-  // if (prefixed) {
-  //   client.on('messageCreate', async message => {
-  //     if (!message.content.startsWith(prefix) || message.author.bot) return;
-  //     const args = message.content.slice(1).trim().split(' ').filter(str => str !== '');
-  //     const command = args.shift().toLowerCase();
-  
-  //     if (!aliases.includes(command)) return;
-  //     if (!message.member.permissions.has(requiredPerms)) return message.react(emotes.error);
-  //     
-  //     callbackParams.action = message;
-  //     callbackParams.userId = await (async () => {
-  //       try {
-  //         const userId = message.mentions.users.first() === undefined ? args[0].replace(/[\\<>@#&!]/g, '') : message.mentions.users.first().id;
-  //         if (!userId.match(/^[0-9]{15,18}/)) return null;
-  //         return userId;
-  //       } catch { return null; }
-  //     })();
-  //     callbackParams.duration = (args[1] !== args.at(-1) && /^\d+(min|h|d|w|m)/.test(args[1]) || /^\d+(min|h|d|w|m)/.test(args[1])) ? args[1] : null;
-  //     callbackParams.reason = args.slice(callbackParams.duration == null ? 1 : 1 + args.indexOf(callbackParams.duration)).join(' ') || null;
-  //     callbackParams.moderator = message.author;
-  //     callbackParams.guild = message.guild;
-  //     callbackParams.messageCount = callbackParams.userId == null ? args[0] : args[1];
-
-  //     await callback(callbackParams);
-  //   });
-  // }
-
-  // if (slash) {
-  //   updateSlashCommands(slashData, aliases[0]);
-  // 
-  //   client.on('interactionCreate', async interaction => {
-  //     if (!interaction.isCommand()) return;
-  //     if (!aliases.includes(interaction.commandName)) return;
-  //     if (!interaction.member.permissions.has(requiredPerms)) return interaction.reply({ content: 'Insufficient Permissions', ephemeral: true });
-  //     // console.log(interaction.options._hoistedOptions);
-  //     
-  //     callbackParams.action = interaction;
-  //     callbackParams.userId = interaction.options.get('user') == null ? null : interaction.options.get('user').value;
-  //     callbackParams.duration = interaction.options.get('duration') == null ? null : interaction.options.get('duration').value;
-  //     callbackParams.reason = interaction.options.get('reason') == null ? null : interaction.options.get('reason').value;
-  //     callbackParams.moderator = interaction.member.user;
-  //     callbackParams.guild = interaction.guild;
-  //     callbackParams.messageCount = interaction.options.get('message_count') == null ? null : interaction.options.get('message_count').value; 
-  //     await callback(callbackParams);
-  //   });
-  //  }
-  // }
 }
 
 export async function initializeCommands(client, commandArray) {
@@ -80,9 +33,6 @@ export async function initializeCommands(client, commandArray) {
     const args = message.content.slice(1).trim().split(' ').filter(str => str !== '');
     const command = args.shift().toLowerCase();
 
-    // if (!aliases.includes(command)) return;
-    // if (!message.member.permissions.has(requiredPerms)) return message.react(emotes.error);
-    
     callbackParams.action = message;
     callbackParams.userId = await (async () => {
       try {
@@ -107,9 +57,7 @@ export async function initializeCommands(client, commandArray) {
 
   client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
-    // if (!aliases.includes(interaction.commandName)) return;
-    // if (!interaction.member.permissions.has(requiredPerms)) return interaction.reply({ content: 'Insufficient Permissions', ephemeral: true });
-    // console.log(interaction.options._hoistedOptions);
+
     callbackParams.action = interaction;
     callbackParams.userId = interaction.options.get('user') == null ? null : interaction.options.get('user').value;
     callbackParams.duration = interaction.options.get('duration') == null ? null : interaction.options.get('duration').value;
